5c2162562ff210b17d89a5b5b588762fc3e56efb
Harsh Shahrma <sharmahs017@gmail.com>
2024-07-16 13:08:59 +0530

    Edited

--- 02CustomDataStructure/05_Linked_list.js
+++ 02CustomDataStructure/05_Linked_list.js
@@ -19,6 +19,8 @@ class LinkedList{
         return this.size
     }
 
+// Time complecity of prepend is O(1)
+
     prepend(value){
         const node = new Node(value)
         if(this.isEmpty()){
@@ -29,13 +31,19 @@ class LinkedList{
         }
         this.size++
     }
+    
+// Time complecity of append is O(n)
 
     append(value){
         const node = new Node(value)
         if(this.isEmpty()){
             this.head = node 
         } else {
-            
+            let prev = this.head
+            while(prev.next){
+                prev = prev.next
+            }
+            prev.next = node
         }
         this.size++
     }
@@ -50,7 +58,7 @@ class LinkedList{
                 listValues += `${curr.value}`
                 curr = curr.next
             }
-            console.log( listValues);
+            console.log(listValues);
         }
     }
 
@@ -62,9 +70,21 @@ console.log(`Is list empty: ${list.isEmpty()}`);
 console.log(`List Size: ${list.getSize()}`);
 list.print()
 
-list.prepend(10)
+// list.prepend(10)
+// list.print()
+
+// list.prepend(20)
+// list.prepend(30)
+// list.print()
+
+list.append(10)
 list.print()
+
 
 list.prepend(20)
 list.prepend(30)
+
+list.append(20)
+list.append(30)
+
 list.print()
